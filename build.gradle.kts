726c33943aea5705f350a8d1fd2e207796c14314
jianh <johnkoghost@gmail.com>
2026-04-26 12:42:52 +0800

    Fix: Normalize plugins and add clean step to resolve resource mutation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.2.2"
-    kotlin("android") version "1.9.22"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
 }
 
 group = "org.example"
