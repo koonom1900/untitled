23a3dec66739966a24425e9f01f82e2a3843d8c7
jianh <johnkoghost@gmail.com>
2026-04-26 11:41:20 +0800

    to Android

---
+++ src/main/kotlin/org/example/MainActivity.kt
@@ -0,0 +1,14 @@
+package org.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Hello, Android!"
+        setContentView(textView)
+    }
+}
